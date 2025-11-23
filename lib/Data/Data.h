b7f931907c3219aea042c50a16111548a40ccb9a
Igor Poteev <48389923+GarryGans@users.noreply.github.com>
2025-11-23 18:44:51 +0300

    Update Data.h

--- lib/Data/Data.h
+++ lib/Data/Data.h
@@ -9,7 +9,7 @@
 // #define minSunRiseBright 58 // min 58 197(340mA) sunRise, 216(120mA) sunSet, max 22(2700ma)
 // #define minSunSetBright 39  // min 39
 
-#define minSunRiseBright 42 // // Cree     Rise 43PWM 31,7V    Max 233PWM 35,7v      Set 38 31.6V
+#define minSunRiseBright 42 // //    Rise 43PWM 31,7V    Max 233PWM 35,7v      Set 38 31.6V
 #define minSunSetBright 37  // 
 
 #define minManual 0
